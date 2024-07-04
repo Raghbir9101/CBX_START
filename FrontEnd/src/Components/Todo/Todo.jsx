d511e51ca01b078ca192096fa4e1a1e1a754b5cc
Raghbir Singh <rkgaming0203@gmail.com>
2024-07-04 17:30:16 +0530

    UI changes

--- FrontEnd/src/Components/Todo/Todo.jsx
+++ FrontEnd/src/Components/Todo/Todo.jsx
@@ -1,86 +1,3 @@
-// // import { Box, TextField, InputAdornment, IconButton, Checkbox, Typography } from '@mui/material';
-// // import React, { useEffect, useLayoutEffect, useState } from 'react';
-// // import AddIcon from '@mui/icons-material/Add';
-// // import { v4 as uid } from "uuid"
-
-// // function Todo() {
-// //     const [tasks, setTasks] = useState([]);
-// //     const [newTask, setNewTask] = useState("");
-// //     const [completePercentage, setCompletePercentage] = useState(0);
-
-// //     const handleAddTask = (e) => {
-// //         setTasks((p) => [...p, {
-// //             _id: uid(),
-// //             task: newTask,
-// //             completed: false
-// //         }])
-// //         setNewTask("")
-// //     }
-
-// //     useEffect(() => {
-// //         let completedTasks = tasks.filter(item => item.completed);
-// //         let completedTasksPercentage = (completedTasks.length / tasks.length) * 100;
-// //         setCompletePercentage(isNaN(completedTasksPercentage) ? 0 : completedTasksPercentage)
-// //     }, [tasks])
-
-// //     return (
-// //         <Box minHeight={"50px"} p={"10px"}>
-// //             <Box display={"flex"} gap={"10px"} alignItems={"center"}>
-// //                 <Typography width={"40px"}>
-// //                     {completePercentage.toFixed(2)}
-// //                 </Typography>
-// //                 <Box flex={1} height={"5px"} bgcolor={"#b1b1b1"}>
-// //                     <Box sx={{transition:".3s"}} height={"100%"} width={`${completePercentage}%`} bgcolor={"#70fe70"}>
-
-// //                     </Box>
-// //                 </Box>
-// //             </Box>
-// //             {
-// //                 tasks.map((item, index) => {
-// //                     return <Box key={item._id} display={"flex"} gap={"5px"} alignItems={"center"}>
-// //                         <Checkbox checked={item.completed} onChange={(e) => {
-// //                             setTasks(p => {
-// //                                 let temp = [...p];
-// //                                 for (let i = 0; i < temp.length; i++) {
-// //                                     if (temp[i]._id == item._id) {
-// //                                         temp[i].completed = !temp[i].completed;
-// //                                         break
-// //                                     }
-// //                                 }
-// //                                 return temp.sort((a, b) => {
-// //                                     return a.completed - b.completed
-// //                                 });
-// //                             })
-// //                         }} />
-// //                         <Typography>{item.task || ""}</Typography>
-// //                     </Box>
-// //                 })
-// //             }
-// //             <TextField
-// //                 size='small'
-// //                 placeholder='Add new Task'
-// //                 sx={{ width: "100%" }}
-// //                 value={newTask}
-// //                 onChange={((e) => {
-// //                     setNewTask(e.target.value)
-// //                 })}
-// //                 InputProps={{
-// //                     endAdornment: (
-// //                         <InputAdornment position="end">
-// //                             <IconButton onClick={handleAddTask} size='small'>
-// //                                 <AddIcon />
-// //                             </IconButton>
-// //                         </InputAdornment>
-// //                     ),
-// //                 }}
-// //             />
-// //         </Box>
-// //     );
-// // }
-
-// // export default Todo;
-
-
 import { Box, TextField, InputAdornment, IconButton, Checkbox, Typography } from '@mui/material';
 import React, { useEffect, useState } from 'react';
 import AddIcon from '@mui/icons-material/Add';
@@ -123,26 +40,24 @@ function Todo({ data, onChange }) {
     };
 
     return (
-        <Box minHeight={"50px"} p={"10px"} width={"100%"} >
+        <Box minHeight={"50px"} p={"20px"} width={"100%"} >
             <Box display={"flex"} gap={"10px"} alignItems={"center"}>
-                <Typography width={"40px"}>
-                    {completePercentage.toFixed(2)}
-                </Typography>
-                <Box flex={1} height={"5px"} bgcolor={"#b1b1b1"}>
-                    <Box sx={{ transition: ".3s" }} height={"100%"} width={`${completePercentage}%`} bgcolor={"#70fe70"}>
+                <Box flex={1} height={"30px"} bgcolor={"rgba(240,240,240)"}  borderRadius={"30px"}>
+                    <Box sx={{ transition: ".3s" }} overflow={"hidden"} height={"100%"} color={"white"} borderRadius={"30px"} width={`${completePercentage}%`} bgcolor={"#B4D33B"} display={"flex"} justifyContent={"center"} alignItems={"center"}>
+                        {completePercentage.toFixed(2)}%
                     </Box>
                 </Box>
             </Box>
-            <Box>
+            <Box mt={"20px"}>
                 <DragDropContext onDragEnd={onDragEnd}>
                     <Droppable droppableId="tasks">
                         {(provided) => (
-                            <Box {...provided.droppableProps} ref={provided.innerRef} sx={{ display: "flex", flexDirection: "column", gap: "5px" }}>
+                            <Box p={"10px"} {...provided.droppableProps} ref={provided.innerRef} sx={{ display: "flex", flexDirection: "column", gap: "5px" }}>
                                 {tasks.map((item, index) => (
                                     <Draggable key={item._id} draggableId={item._id} index={index}>
                                         {(provided) => (
                                             <Box
-                                                sx={{ bgcolor: "#f0f0f0" }}
+                                                // sx={{ bgcolor: "#f0f0f0" }}
                                                 ref={provided.innerRef}
                                                 {...provided.draggableProps}
                                                 {...provided.dragHandleProps}
@@ -154,10 +69,8 @@ function Todo({ data, onChange }) {
                                             // mb={1}
                                             >
                                                 <Box width={"15%"}>
-                                                    <Checkbox
-                                                        size='small'
-                                                        checked={item.completed}
-                                                        onChange={() => {
+                                                    <IconButton
+                                                        onClick={() => {
                                                             setTasks(p => {
                                                                 let temp = [...p];
                                                                 for (let i = 0; i < temp.length; i++) {
@@ -169,12 +82,31 @@ function Todo({ data, onChange }) {
                                                                 return temp.sort((a, b) => a.completed - b.completed);
                                                             });
                                                         }}
-                                                    />
+                                                    >
+                                                        {
+                                                            item.completed ?
+                                                                <svg width="17" height="18" viewBox="0 0 17 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                                    <rect x="0.314453" y="0.827148" width="16.2963" height="16.2963" rx="3.05556" fill="#B4D33B" />
+                                                                </svg>
+                                                                : <svg width="17" height="18" viewBox="0 0 17 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                                    <rect x="0.823712" y="1.28074" width="15.2778" height="15.2778" rx="2.5463" stroke="#989696" stroke-width="1.01852" />
+                                                                </svg>
+
+                                                        }
+
+                                                    </IconButton>
                                                 </Box>
-                                                <Typography whiteSpace={"wrap"} maxWidth={"70%"} sx={{ textWrap: "wrap", overflowWrap: "break-word" }} flex={1}>{item.task || ""}</Typography>
+                                                <Typography style={{ textDecoration: item.completed ? "line-through" : "none" }} whiteSpace={"wrap"} maxWidth={"70%"} sx={{ textWrap: "wrap", overflowWrap: "break-word" }} flex={1}>{item.task || ""}</Typography>
                                                 <Box width={"15%"}>
                                                     <IconButton onClick={() => handleDeleteTask(item._id)} size='small'>
-                                                        <DeleteIcon />
+                                                        <svg width="16" height="17" viewBox="0 0 16 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                            <path d="M0.884277 3.70605H14.6343" stroke="#616161" stroke-width="0.900086" stroke-linecap="round" stroke-linejoin="round" />
+                                                            <path d="M13.1066 3.70605V14.4005C13.1066 15.1644 12.3427 15.9283 11.5788 15.9283H3.93989C3.176 15.9283 2.41211 15.1644 2.41211 14.4005V3.70605" stroke="#616161" stroke-width="0.900086" stroke-linecap="round" stroke-linejoin="round" />
+                                                            <path d="M4.70361 3.70595V2.17817C4.70361 1.41428 5.4675 0.650391 6.23139 0.650391H9.28695C10.0508 0.650391 10.8147 1.41428 10.8147 2.17817V3.70595" stroke="#616161" stroke-width="0.900086" stroke-linecap="round" stroke-linejoin="round" />
+                                                            <path d="M6.23145 7.52539V12.1087" stroke="#616161" stroke-width="0.900086" stroke-linecap="round" stroke-linejoin="round" />
+                                                            <path d="M9.28711 7.52539V12.1087" stroke="#616161" stroke-width="0.900086" stroke-linecap="round" stroke-linejoin="round" />
+                                                        </svg>
+
                                                     </IconButton>
                                                 </Box>
                                             </Box>
@@ -187,7 +119,8 @@ function Todo({ data, onChange }) {
                     </Droppable>
                 </DragDropContext>
             </Box>
-            <TextField
+            
+            {/* <TextField
                 size='small'
                 placeholder='Add new Task'
                 sx={{ width: "100%", mt: "10px" }}
@@ -202,7 +135,7 @@ function Todo({ data, onChange }) {
                         </InputAdornment>
                     ),
                 }}
-            />
+            /> */}
         </Box>
     );
 }
