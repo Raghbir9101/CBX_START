f00444301bc93918c340955463cc9f328e55aa3b
jayprakashlohar <103755935+jayprakashlohar@users.noreply.github.com>
2024-07-17 14:12:51 +0530

    updating admin dashboard ui

---
+++ FrontEnd/src/Components/AdminPanel/AdminNavbar.jsx
@@ -0,0 +1,90 @@
+import React, { useContext, useState } from "react";
+import { Box, Menu, MenuItem } from "@mui/material";
+import { Context } from "../Context/Context";
+import DefaultPFP from "../../Icons/Default_pfp.jpg";
+import { useNavigate } from "react-router-dom";
+
+const AdminNavbar = () => {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const { token, loginUser, handleLogout } = useContext(Context);
+  const nav = useNavigate();
+  const open = Boolean(anchorEl);
+  const handleClickMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleCloseMenu = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <Box className="navMainBox">
+      <Box
+        display={"flex"}
+        alignItems={"center"}
+        justifyContent={"space-between"}
+        sx={{ width: "100%" }}
+      >
+        <Box width={"33%"}>
+          <div className="group">
+            <img
+              className="image"
+              alt="Image"
+              src="https://c.animaapp.com/YKPFj7gL/img/image-8@2x.png"
+            />
+          </div>
+        </Box>
+
+        {token && (
+          <Box
+            width={"33%"}
+            sx={{
+              display: "flex",
+              justifyContent: "flex-end",
+              alignItems: "center",
+              gap: "30px",
+            }}
+          >
+            <Box onClick={handleClickMenu}>
+              <img
+                style={{
+                  objectFit: "cover",
+                  borderRadius: "100%",
+                  width: "40px",
+                  height: "40px",
+                  cursor: "pointer",
+                }}
+                src={loginUser?.photo || ""}
+                onError={(e) => (e.target.src = DefaultPFP)}
+                alt="profile"
+              />
+            </Box>
+          </Box>
+        )}
+        <Menu
+          id="simple-menu"
+          anchorEl={anchorEl}
+          open={open}
+          onClose={handleCloseMenu}
+          sx={{
+            ".MuiPaper-root": {
+              borderRadius: "16px",
+            },
+          }}
+        >
+          <MenuItem>{loginUser?.userName || ""}</MenuItem>
+          <MenuItem>{loginUser?.email || ""}</MenuItem>
+          <MenuItem
+            onClick={() => {
+              handleLogout();
+              nav("/login");
+            }}
+          >
+            Logout
+          </MenuItem>
+        </Menu>
+      </Box>
+    </Box>
+  );
+};
+
+export default AdminNavbar;
--- FrontEnd/src/Components/AdminPanel/AdminPanel.jsx
+++ FrontEnd/src/Components/AdminPanel/AdminPanel.jsx
@@ -1,43 +1,56 @@
-import React, { useContext, useEffect, useState } from 'react'
-import { Box, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Checkbox, Avatar, Button, TablePagination, Menu, MenuItem } from '@mui/material';
-import HTTP from "../../HTTP"
-import { Context } from '../Context/Context';
-import DefaultPFP from "../../Icons/Default_pfp.jpg"
-import { useNavigate } from 'react-router-dom';
+import React, { useContext, useEffect, useState } from "react";
+import {
+  Box,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+  Checkbox,
+  Avatar,
+  TablePagination,
+  IconButton,
+  Tooltip,
+} from "@mui/material";
+import HTTP from "../../HTTP";
+import { Context } from "../Context/Context";
+import DeleteOutlinedIcon from "@mui/icons-material/DeleteOutlined";
+import "./AdminPanel.css";
+import AdminNavbar from "./AdminNavbar";
+
 const styles = {
   tableContainer: {
-    marginTop: '20px',
+    marginTop: "20px",
   },
   tableHeaderCell: {
-    fontWeight: 'bold',
+    fontWeight: "bold",
+    fontSize: "15px",
   },
   approveCheckbox: {
-    color: 'green',
+    color: "green",
   },
   adminCheckbox: {
-    color: 'blue',
+    color: "blue",
   },
   avatar: {
-    width: 50,
-    height: 50,
-  },
-  deleteButton: {
-    color: 'red',
+    width: 35,
+    height: 35,
   },
 };
-
-
 
 function AdminPanel() {
   const [users, setUsers] = useState([]);
   const [page, setPage] = useState(0);
-  const [rowsPerPage, setRowsPerPage] = useState(5);
-  const { token, loginUser } = useContext(Context)
+  const [rowsPerPage, setRowsPerPage] = useState(20);
+  const { token, loginUser } = useContext(Context);
+
   const handleApprovalChange = async (event, userId) => {
     await HTTP.patch(`updateUsersAdmin/${userId}`, {
-      isApproved: event.target.checked
-    })
-    const updatedUsers = users.map(user =>
+      isApproved: event.target.checked,
+    });
+    const updatedUsers = users.map((user) =>
       user._id === userId ? { ...user, isApproved: event.target.checked } : user
     );
     setUsers(updatedUsers);
@@ -45,17 +58,17 @@ function AdminPanel() {
 
   const handleAdminChange = async (event, userId) => {
     await HTTP.patch(`updateUsersAdmin/${userId}`, {
-      isAdmin: event.target.checked
-    })
-    const updatedUsers = users.map(user =>
+      isAdmin: event.target.checked,
+    });
+    const updatedUsers = users.map((user) =>
       user._id === userId ? { ...user, isAdmin: event.target.checked } : user
     );
     setUsers(updatedUsers);
   };
 
   const handleDeleteUser = async (userId) => {
-    await HTTP.delete(`deleteUsersAdmin/${userId}`)
-    const updatedUsers = users.filter(user => user._id !== userId);
+    await HTTP.delete(`deleteUsersAdmin/${userId}`);
+    const updatedUsers = users.filter((user) => user._id !== userId);
     setUsers(updatedUsers);
   };
 
@@ -70,62 +83,130 @@ function AdminPanel() {
 
   useEffect(() => {
     if (!token || loginUser?.isAdmin == false) {
-      return
+      return;
     }
     HTTP.get(`getUsersAdmin`).then((res) => {
-      setUsers(res.data)
-    })
-  }, [])
+      setUsers(res.data);
+    });
+  }, []);
+
   return (
     <Box display={"flex"} flexDirection={"column"}>
-      <Nav />
-      <Box width={"100%"} padding={"20px"}>
+      <AdminNavbar />
+      <Box width={"90%"} m={"auto"} pt={"20px"}>
         <TableContainer component={Paper} style={styles.tableContainer}>
           <Table>
             <TableHead>
-              <TableRow>
-                <TableCell style={styles.tableHeaderCell}>Photo</TableCell>
-                <TableCell style={styles.tableHeaderCell}>User Name</TableCell>
-                <TableCell style={styles.tableHeaderCell}>Email</TableCell>
-                <TableCell style={styles.tableHeaderCell}>Is Approved</TableCell>
-                <TableCell style={styles.tableHeaderCell}>Is Admin</TableCell>
-                <TableCell style={styles.tableHeaderCell}>Actions</TableCell>
+              <TableRow sx={{ background: "#f3f3f3" }}>
+                <TableCell
+                  style={styles.tableHeaderCell}
+                  sx={{ textAlign: "center" }}
+                >
+                  Image
+                </TableCell>
+                <TableCell
+                  style={styles.tableHeaderCell}
+                  sx={{ textAlign: "left", pl: 0 }}
+                >
+                  User Name
+                </TableCell>
+                <TableCell
+                  style={styles.tableHeaderCell}
+                  sx={{ textAlign: "left", pl: 0 }}
+                >
+                  Email
+                </TableCell>
+                <TableCell
+                  style={styles.tableHeaderCell}
+                  sx={{ textAlign: "center" }}
+                >
+                  Is Approved
+                </TableCell>
+                <TableCell
+                  style={styles.tableHeaderCell}
+                  sx={{ textAlign: "center" }}
+                >
+                  Is Admin
+                </TableCell>
+                <TableCell
+                  style={styles.tableHeaderCell}
+                  sx={{ textAlign: "center" }}
+                >
+                  Actions
+                </TableCell>
               </TableRow>
             </TableHead>
             <TableBody>
               {(rowsPerPage > 0
-                ? users.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                ? users.slice(
+                    page * rowsPerPage,
+                    page * rowsPerPage + rowsPerPage
+                  )
                 : users
               ).map((user) => (
                 <TableRow key={user._id}>
-                  <TableCell>
-                    <Avatar alt={user.userName} src={user.photo} style={styles.avatar} />
+                  <TableCell
+                    className="tableCell"
+                    sx={{
+                      textAlign: "center",
+                    }}
+                  >
+                    <Box
+                      sx={{
+                        display: "flex",
+                        justifyContent: "center",
+                      }}
+                    >
+                      {" "}
+                      <Avatar
+                        alt={user.userName}
+                        src={user.photo}
+                        style={styles.avatar}
+                      />
+                    </Box>
                   </TableCell>
-                  <TableCell>{user.userName}</TableCell>
-                  <TableCell>{user.email}</TableCell>
-                  <TableCell>
+                  <TableCell
+                    sx={{ textAlign: "left", pl: 0 }}
+                    className="tableCell"
+                  >
+                    {user.userName}
+                  </TableCell>
+                  <TableCell
+                    sx={{ textAlign: "left", pl: 0 }}
+                    className="tableCell"
+                  >
+                    {user.email}
+                  </TableCell>
+                  <TableCell sx={{ textAlign: "center" }} className="tableCell">
                     <Checkbox
+                      size="small"
                       defaultChecked={user.isApproved}
-                      onChange={(event) => handleApprovalChange(event, user._id)}
+                      onChange={(event) =>
+                        handleApprovalChange(event, user._id)
+                      }
                       style={styles.approveCheckbox}
                     />
                   </TableCell>
-                  <TableCell>
+                  <TableCell sx={{ textAlign: "center" }} className="tableCell">
                     <Checkbox
+                      size="small"
                       defaultChecked={user.isAdmin}
                       onChange={(event) => handleAdminChange(event, user._id)}
                       style={styles.adminCheckbox}
                     />
                   </TableCell>
-                  <TableCell>
-                    <Button
-                      variant="outlined"
-                      color="secondary"
-                      style={styles.deleteButton}
-                      onClick={() => handleDeleteUser(user._id)}
-                    >
-                      Delete
-                    </Button>
+                  <TableCell sx={{ textAlign: "center" }} className="tableCell">
+                    <Tooltip title="Delete" arrow placement="right">
+                      <IconButton onClick={() => handleDeleteUser(user._id)}>
+                        <DeleteOutlinedIcon
+                          style={{
+                            color: "red",
+                            width: "20px",
+                            height: "20px",
+                          }}
+                        />
+                      </IconButton>
+                    </Tooltip>
                   </TableCell>
                 </TableRow>
               ))}
@@ -133,7 +214,7 @@ function AdminPanel() {
           </Table>
         </TableContainer>
         <TablePagination
-          rowsPerPageOptions={[5, 10, 25]}
+          rowsPerPageOptions={[20, 25, 50]}
           component="div"
           count={users.length}
           rowsPerPage={rowsPerPage}
@@ -143,91 +224,7 @@ function AdminPanel() {
         />
       </Box>
     </Box>
-  )
+  );
 }
 
-export default AdminPanel
-
-
-
-function Nav() {
-  const [anchorEl, setAnchorEl] = useState(null);
-  const { token, loginUser, handleLogout } = useContext(Context)
-  const nav = useNavigate()
-  const open = Boolean(anchorEl);
-  const handleClickMenu = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
-  const handleCloseMenu = () => {
-    setAnchorEl(null);
-  };
-
-
-  return <Box className="navMainBox">
-    <Box
-      display={"flex"}
-      alignItems={"center"}
-      justifyContent={"space-between"}
-      sx={{ width: "100%" }}
-    >
-      <Box width={"33%"}>
-        <div className="group">
-          <img
-            className="image"
-            alt="Image"
-            src="https://c.animaapp.com/YKPFj7gL/img/image-8@2x.png"
-          />
-        </div>
-      </Box>
-
-      {token && (
-        <Box
-          width={"33%"}
-          sx={{
-            display: "flex",
-            justifyContent: "flex-end",
-            alignItems: "center",
-            gap: "30px",
-          }}
-        >
-          <Box onClick={handleClickMenu}>
-            <img
-              style={{
-                objectFit: "cover",
-                borderRadius: "100%",
-                width: "40px",
-                height: "40px",
-                cursor: "pointer",
-              }}
-              src={loginUser?.photo || ""}
-              onError={(e) => e.target.src = DefaultPFP}
-              alt="profile"
-            />
-          </Box>
-        </Box>
-      )}
-      <Menu
-        id="simple-menu"
-        anchorEl={anchorEl}
-        open={open}
-        onClose={handleCloseMenu}
-        sx={{
-          ".MuiPaper-root": {
-            borderRadius: "16px",
-          },
-        }}
-      >
-        <MenuItem>{loginUser?.userName || ""}</MenuItem>
-        <MenuItem>{loginUser?.email || ""}</MenuItem>
-        <MenuItem
-          onClick={() => {
-            handleLogout();
-            nav("/login");
-          }}
-        >
-          Logout
-        </MenuItem>
-      </Menu>
-    </Box>
-  </Box>
-}
+export default AdminPanel;
