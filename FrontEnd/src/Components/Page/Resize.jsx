9829ce5765ea0e2e1dc678da5b8276b91f296a7b
jayprakashlohar <103755935+jayprakashlohar@users.noreply.github.com>
2024-07-26 12:36:25 +0530

    update divider height

--- FrontEnd/src/Components/Page/Resize.jsx
+++ FrontEnd/src/Components/Page/Resize.jsx
@@ -46,7 +46,6 @@ function HorizontalResizableDiv({ columnId, onResize }) {
       onResize(`${newParentWidth}px`);
     };
 
-
     if (resizing) {
       window.addEventListener("mousemove", handleMouseMove);
       window.addEventListener("mouseup", handleMouseUp);
@@ -78,26 +77,74 @@ function HorizontalResizableDiv({ columnId, onResize }) {
       bgcolor="transparent"
       position={"absolute"}
       right={"-15px"}
-      sx={{ cursor: "col-resize", width: "20px" }}
+      sx={{ cursor: "col-resize", width: "20px", mt: "30px" }}
       onDrag={(e) => e.preventDefault()}
       onMouseDown={handleMouseDown}
       draggable={false}
       onMouseMove={(e) => {
         const box = e.currentTarget.getBoundingClientRect();
         const offsetY = e.clientY - box.top;
-        btnRef.current.style.top = `${offsetY - btnRef.current.offsetHeight / 2}px`
+        btnRef.current.style.top = `${
+          offsetY - btnRef.current.offsetHeight / 2
+        }px`;
       }}
     >
-      <Box sx={{ position: "absolute", width:"10px", height:"10px", right: 0, top: "0", transform: `translate(50%, 0%)`, bgcolor: "#B4D33B", borderRadius: "50%" }}>
-
-      </Box>
-      <Box sx={{ position: "absolute", width:"10px", height:"10px", right: 0, bottom: "0", transform: `translate(50%, 0%)`, bgcolor: "#B4D33B", borderRadius: "50%" }}>
-
-      </Box>
-      <IconButton ref={btnRef} size="small" sx={{ cursor: "col-resize", position: "absolute", right: 0, top: "30%", transform: `translate(50%, 0%)`, bgcolor: "white", "&:hover": { bgcolor: "white" } }}>
-        <svg width="12" height="10" viewBox="0 0 12 10" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M3.8151 1.54199L1.26855 5.17992L3.8151 8.81784" stroke="black" stroke-width="1.25727" stroke-linecap="round" />
-          <path d="M8.54427 1.54199L11.0908 5.17992L8.54427 8.81784" stroke="black" stroke-width="1.25727" stroke-linecap="round" />
+      <Box
+        sx={{
+          position: "absolute",
+          width: "10px",
+          height: "10px",
+          right: 0,
+          top: "0",
+          transform: `translate(50%, 0%)`,
+          bgcolor: "#B4D33B",
+          borderRadius: "50%",
+        }}
+      ></Box>
+      <Box
+        sx={{
+          position: "absolute",
+          width: "10px",
+          height: "10px",
+          right: 0,
+          bottom: "0",
+          transform: `translate(50%, 0%)`,
+          bgcolor: "#B4D33B",
+          borderRadius: "50%",
+        }}
+      ></Box>
+      <IconButton
+        ref={btnRef}
+        size="small"
+        sx={{
+          cursor: "col-resize",
+          position: "absolute",
+          right: 0,
+          top: "30%",
+          transform: `translate(50%, 0%)`,
+          bgcolor: "white",
+          "&:hover": { bgcolor: "white" },
+        }}
+      >
+        <svg
+          width="12"
+          height="10"
+          viewBox="0 0 12 10"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M3.8151 1.54199L1.26855 5.17992L3.8151 8.81784"
+            stroke="black"
+            stroke-width="1.25727"
+            stroke-linecap="round"
+          />
+          <path
+            d="M8.54427 1.54199L11.0908 5.17992L8.54427 8.81784"
+            stroke="black"
+            stroke-width="1.25727"
+            stroke-linecap="round"
+          />
         </svg>
       </IconButton>
     </Box>
