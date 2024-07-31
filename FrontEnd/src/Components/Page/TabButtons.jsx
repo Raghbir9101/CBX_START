f4fa274d1297c7573de4d369dd2dc00388ddb716
Raghbir Singh <rkgaming0203@gmail.com>
2024-07-31 19:17:07 +0530

    add

--- FrontEnd/src/Components/Page/TabButtons.jsx
+++ FrontEnd/src/Components/Page/TabButtons.jsx
@@ -319,7 +319,7 @@ const TabButtons = ({
 
               }}
             >
-              <AddIcon sx={{ width: "16px", height: "16px" }} />
+              <AddIcon sx={{ width: "10px", height: "10px" }} />
             </IconButton>
           </span>
         </Box>
