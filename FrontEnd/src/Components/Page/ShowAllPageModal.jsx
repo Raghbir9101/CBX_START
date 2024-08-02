def4bae2e8fc7c70386e7346444bdc5653703c37
jayprakashlohar <103755935+jayprakashlohar@users.noreply.github.com>
2024-08-02 18:46:45 +0530

    added close icon

--- FrontEnd/src/Components/Page/ShowAllPageModal.jsx
+++ FrontEnd/src/Components/Page/ShowAllPageModal.jsx
@@ -15,7 +15,7 @@ import DeleteModal from "./DeleteModal";
 import { Context } from "../Context/Context";
 import { useNavigate, useParams } from "react-router-dom";
 import CloseOutlinedIcon from "@mui/icons-material/CloseOutlined";
-import ContentCopyIcon from '@mui/icons-material/ContentCopy';
+import ContentCopyIcon from "@mui/icons-material/ContentCopy";
 import DuplicateModal from "./DuplicateModal";
 const modalStyle = {
   minWidth: 450,
@@ -24,6 +24,8 @@ const modalStyle = {
   height: "fit-content",
   p: 4,
   borderRadius: "18px",
+  outline: "none",
+  position: "relative",
 };
 
 const ShowAllPageModal = ({
@@ -43,7 +45,7 @@ const ShowAllPageModal = ({
   };
   const handleCloseDeleteModal = () => {
     setOpenDeleteModal(false);
-    setSelectedPage(null)
+    setSelectedPage(null);
   };
   const [openDuplicateModal, setOpenDuplicateModal] = useState(false);
   const handleOpenDuplicateModal = () => {
@@ -51,7 +53,7 @@ const ShowAllPageModal = ({
   };
   const handleCloseDuplicateModal = () => {
     setOpenDuplicateModal(false);
-    setSelectedPage(null)
+    setSelectedPage(null);
   };
   const { pageID } = useParams();
   const nav = useNavigate();
@@ -74,15 +76,37 @@ const ShowAllPageModal = ({
         open={open}
         onClose={handleClose}
         aria-labelledby="create-page-modal-title"
-        aria-describedby="create-page-modal-description" sx={{ overflowY: "auto", padding: "20px", display: "flex", justifyContent: "center" }}
+        aria-describedby="create-page-modal-description"
+        sx={{
+          overflowY: "auto",
+          padding: "20px",
+          display: "flex",
+          justifyContent: "center",
+        }}
       >
         <Box sx={modalStyle}>
           <Box>
             <Box
               sx={{
+                position: "absolute",
+                top: "8px",
+                right: "10px",
+              }}
+            >
+              <Tooltip title="Close Modal" arrow placement="left">
+                <IconButton size="small" onClick={handleClose}>
+                  <CloseOutlinedIcon
+                    sx={{ color: "#868686", width: "20px", height: "20px" }}
+                  />
+                </IconButton>
+              </Tooltip>
+            </Box>
+            <Box
+              sx={{
                 display: "flex",
                 alignItems: "center",
                 position: "relative",
+                mt: 1,
               }}
             >
               <input
@@ -102,18 +126,20 @@ const ShowAllPageModal = ({
                   left: "10px",
                 }}
               />
-              <IconButton
-                className="searchIcon"
-                size="small"
-                onClick={handleClear}
-              >
-                {/* <SearchRoundedIcon
+              {searchQuery && (
+                <IconButton
+                  className="searchIcon"
+                  size="small"
+                  onClick={handleClear}
+                >
+                  {/* <SearchRoundedIcon
                   sx={{ color: "#ACB1C6", width: "20px", height: "20px" }}
                 /> */}
-                <CloseOutlinedIcon
-                  sx={{ color: "#ACB1C6", width: "20px", height: "20px" }}
-                />
-              </IconButton>
+                  <CloseOutlinedIcon
+                    sx={{ color: "#ACB1C6", width: "20px", height: "20px" }}
+                  />
+                </IconButton>
+              )}
             </Box>
 
             <Box sx={{ mt: 2 }}>
@@ -138,7 +164,7 @@ const ShowAllPageModal = ({
                     }}
                     onClick={() => {
                       nav("/page/" + item._id);
-                      handleClose()
+                      handleClose();
                     }}
                   >
                     <Box
@@ -172,27 +198,26 @@ const ShowAllPageModal = ({
                           </IconButton>
                         </Tooltip>
                       )}
-                      {(item.role == "OWNER" || item.role == "EDITOR") &&
-                        (
-                          <Tooltip title="Edit this page" arrow>
-                            <IconButton
-                              size="small"
-                              onClick={(e) => {
-                                e.stopPropagation();
-                                handleEditOpenModal(e);
-                                setSelectedPage(item);
+                      {(item.role == "OWNER" || item.role == "EDITOR") && (
+                        <Tooltip title="Edit this page" arrow>
+                          <IconButton
+                            size="small"
+                            onClick={(e) => {
+                              e.stopPropagation();
+                              handleEditOpenModal(e);
+                              setSelectedPage(item);
+                            }}
+                          >
+                            <EditOutlinedIcon
+                              sx={{
+                                color: "#B5B5B5",
+                                width: "20px",
+                                height: "20px",
                               }}
-                            >
-                              <EditOutlinedIcon
-                                sx={{
-                                  color: "#B5B5B5",
-                                  width: "20px",
-                                  height: "20px",
-                                }}
-                              />
-                            </IconButton>
-                          </Tooltip>
-                        )}
+                            />
+                          </IconButton>
+                        </Tooltip>
+                      )}
                       {(item.role == "OWNER" || item.role == "EDITOR") && (
                         <Tooltip title="Delete this page" arrow>
                           <IconButton
